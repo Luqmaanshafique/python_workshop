2c7c7bf03a53e92add88d7a5fc6e15fc55337c08
Luqmaanshafique <51982169+Luqmaanshafique@users.noreply.github.com>
2019-06-20 12:53:13 +0530

    Add files via upload

--- concatenate.py
+++ concatenate.py
@@ -3,8 +3,10 @@ def concatenate():
     dname=(input("Enter the dbname-"))
     username=(input("Enter the username-"))
     password=(input("Enter the pasword-"))
-    result=("servname="+servername+";dbname="+dname+";uname="+username+";password="+password+";")
+    #result="servname="+servername+";dbname="+dname+";uname="+username+";password="+password+";"
+    result="servername=%s; dbname=%s; uname=%s; password=%s;" %(servername,dname,username,password)
     return result
 
 res=concatenate()    
 print(res)
+
